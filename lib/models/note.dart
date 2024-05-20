2cabba10e2924b096b9425db3b6931bb269938ad
Richie Jonathan Chaniago <richiejonathanchaniago@gmail.com>
2024-05-20 22:44:56 +0700

    online

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,21 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class Note {
   String? id;
   final String title;
-  String? imageUrl;
   final String description;
+  String? imageUrl;
+  double? latitude;
+  double? longitude;
   Timestamp? createdAt;
   Timestamp? updatedAt;
+
 
   Note({
     this.id,
     required this.title,
     required this.description,
     this.imageUrl,
+    this.latitude,
+    this.longitude,
     this.createdAt,
     this.updatedAt,
   });
+
 
   factory Note.fromDocument(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
@@ -23,17 +30,22 @@ class Note {
       id: doc.id,
       title: data['title'],
       description: data['description'],
-      imageUrl: data['image_Url'],
+      imageUrl: data['image_url'],
+      latitude: data['latitude'] as double,
+      longitude: data['longitude'] as double,
       createdAt: data['created_at'] as Timestamp,
       updatedAt: data['updated_at'] as Timestamp,
     );
   }
 
+
   Map<String, dynamic> toDocument() {
     return {
       'title': title,
       'description': description,
-      'image_Url': imageUrl,
+      'image_url': imageUrl,
+      'latitude': latitude,
+      'longitude': longitude,
       'created_at': createdAt,
       'updated_at': updatedAt,
     };
